52fe1642b0973da7f91ddd49633e206f420cd1a5
VALux <olex3000@yandex.ru>
2018-03-01 15:52:12 +0200

    added docker-compose for rabbitmq

--- src/main/java/com/alvo/twitteringestor/producing/TweetAMQPProducingService.java
+++ src/main/java/com/alvo/twitteringestor/producing/TweetAMQPProducingService.java
@@ -3,14 +3,34 @@ package com.alvo.twitteringestor.producing;
 import com.alvo.twitteringestor.model.Tweet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 
 @Service
 public class TweetAMQPProducingService implements TweetProducingService<Tweet> {
   private static final Logger LOGGER = LoggerFactory.getLogger(TweetAMQPProducingService.class);
 
+  private final RabbitTemplate rabbitTemplate;
+  private final ConfigurableApplicationContext context;
+
+  @Inject
+  public TweetAMQPProducingService(RabbitTemplate rabbitTemplate, ConfigurableApplicationContext context) {
+    this.rabbitTemplate = rabbitTemplate;
+    this.context = context;
+  }
+
   @Override
   public void produce(Tweet tweet) {
-    LOGGER.info("Tweet pushed to the queue");
+    LOGGER.info("Tweet {}pushed to the queue", tweet);
+    rabbitTemplate.convertAndSend(tweet);
+  }
+
+  @PreDestroy
+  public void destroy() {
+    context.close();
   }
 }
